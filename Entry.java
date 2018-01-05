037e642c9e1b3695d746d8134c84ec64f4277463
sputnik792 <kevinyuangao@gmail.com>
2018-01-04 23:17:25 -0500

    Added Entry.java

---
+++ Entry.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Entry{
+	private ArrayList<Integer> entries = new ArrayList<Integer>();
+
+	public Entry(){
+		for (int i = 1; i <= 9; i++){
+			entries.add(i);
+		}
+	}
+	
+	public void setEntries(ArrayList<Integer> arr){
+		this.entries = arr;
+	}
+	
+	public ArrayList<Integer> getEntries(){
+		return this.entries;
+	}
+	
+	//removes object e
+	public void deleteObj(Integer e){
+		entries.remove(e);
+	}
+	
+	//removes the object at index e
+	public void deleteIndex(int e){
+		entries.remove(e);
+	}
+	
+	public void remove_all(){
+		while (!(this.entries.isEmpty())){
+			entries.remove(0);
+		}
+	}
+	
+	public boolean isIn(Integer e){
+		return entries.contains(e);
+	}
+	
+	public int get_size(){
+		return entries.size();
+	}
+	
+	public void resetEntries(){
+		if (entries.isEmpty()){
+			for (int i = 1; i <= 9; i++){
+				entries.add(i);
+			}
+		}
+	}
+}
