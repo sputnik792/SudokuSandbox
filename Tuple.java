b67e186bdd489233de2b539d96dc089f8fd72364
Kevin Gao <kevinyuangao@gmail.com>
2018-04-18 15:40:45 -0400

    updated Tuple

--- Tuple.java
+++ Tuple.java
@@ -7,9 +7,13 @@ public class Tuple<X, Y> {
 		this.first = x; 
 		this.second = y; 
 	} 
- 
+
+	public Tuple<Y, X> reverse(Tuple<X, Y> tup) {
+		Tuple<Y, X> newtup = new Tuple<Y, X>(tup.second, tup.first);
+		return newtup;
+	}
+
 	public void print_tuple(Tuple<X, Y> tup){
 		System.out.println("tuple: "+tup.first+","+tup.second);
 	}
-	
 } 
