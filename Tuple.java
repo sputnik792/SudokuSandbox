f0e273f3288d1f8ecd69c69741328ebfba201565
sputnik792 <kevinyuangao@gmail.com>
2018-06-02 03:27:07 -0400

    updated tuple

--- Tuple.java
+++ Tuple.java
@@ -8,12 +8,21 @@ public class Tuple<X, Y> {
 		this.second = y; 
 	} 
 
-	public Tuple<Y, X> reverse(Tuple<X, Y> tup) {
-		Tuple<Y, X> newtup = new Tuple<Y, X>(tup.second, tup.first);
+	public Tuple<Y, X> reverse() {
+		Tuple<Y, X> newtup = new Tuple<Y, X>(this.second, this.first);
 		return newtup;
 	}
 
-	public void print_tuple(Tuple<X, Y> tup){
-		System.out.println("tuple: "+tup.first+","+tup.second);
+	public void print_tuple(){
+		System.out.println("tuple: "+this.first+","+this.second);
+	}
+	
+	public boolean equals(Tuple<X, Y> tup) { //check if two tuples contain the same values
+		if ((this.first == tup.first)&&(this.second == tup.second)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 } 
